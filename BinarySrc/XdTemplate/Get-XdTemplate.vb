f0717b82ad2639e78e02495e3b0582773eed3ca5
mithrandyr <mithrandyr@users.noreply.github.com>
2022-06-03 14:56:38 -0400

    Added cmdlet to search for templates

---
+++ BinarySrc/XdTemplate/Get-XdTemplate.vb
@@ -0,0 +1,56 @@
+﻿<Cmdlet(VerbsCommon.Get, "XdTemplate")>
+<CmdletBinding(DefaultParameterSetName:="search")>
+Public Class Get_XdTemplate
+    Inherits baseCmdlet
+
+    <Parameter(ParameterSetName:="search")>
+    Public Property Search As String
+
+    <Parameter(ParameterSetName:="search")>
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
+    Public Property TemplateLibrary As String
+
+    <Parameter(ParameterSetName:="search")>
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
+    Public Property TemplateGroup As String
+
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
+    Public Property TemplateName As String
+
+    <Parameter(Mandatory:=True, ParameterSetName:="id", ValueFromPipelineByPropertyName:=True)>
+    Public Property TemplateId As Guid
+
+    Protected Overrides Sub EndProcessing()
+        Dim query = xdp.Templates.Expand(Function(x) x.TemplateGroup.TemplateLibrary).AsQueryable
+
+        If Not String.IsNullOrWhiteSpace(TemplateGroup) Then
+            query = query.Where(Function(x) x.TemplateGroup.Name.ToUpper.Equals(TemplateGroup.ToUpper))
+        End If
+
+        If Not String.IsNullOrWhiteSpace(TemplateLibrary) Then
+            query = query.Where(Function(x) x.TemplateGroup.TemplateLibrary.Name.ToUpper.Equals(TemplateLibrary.ToUpper))
+        End If
+
+        If ParameterSetName = "search" Then
+            If Not String.IsNullOrWhiteSpace(Search) Then
+                query = query.Where(Function(x) x.Name.Contains(Search))
+            End If
+            For Each gr In GenerateResults(query, "Template")
+                WriteObject(gr)
+            Next
+        Else
+            Dim t As Template
+            If ParameterSetName = "name" Then
+                t = ExecuteWithTimeout(query.Where(Function(x) x.Name.ToUpper.Equals(TemplateName.ToUpper)).FirstOrDefaultAsync)
+            Else
+                Try
+                    t = ExecuteWithTimeout(query.Where(Function(x) x.TemplateId = TemplateId).FirstOrDefaultAsync)
+                Catch
+                    'TODO: add error handling.  When selecting using a GUID, if the guid doesn't exist, a NotFound error gets thrown.  Currently we are swallowing this error and returning nothing.
+                End Try
+            End If
+
+            If t IsNot Nothing Then WriteObject(t)
+        End If
+    End Sub
+End Class
