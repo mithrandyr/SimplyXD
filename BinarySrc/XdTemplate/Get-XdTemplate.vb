244318681505be680ded8b8491db45555b1d6f76
mithrandyr <mithrandyr@users.noreply.github.com>
2022-06-10 14:43:06 -0400

    Updated Get-XdTemplate to support pipelining (pass in TemplateGroup to get all templates)

--- BinarySrc/XdTemplate/Get-XdTemplate.vb
+++ BinarySrc/XdTemplate/Get-XdTemplate.vb
@@ -3,68 +3,72 @@
 Public Class Get_XdTemplate
     Inherits baseCmdlet
 
-    <Parameter(ParameterSetName:="search")>
+    <Parameter(ParameterSetName:="search", ValueFromPipelineByPropertyName:=True)>
     Public Property Search As String
 
     <Parameter(ParameterSetName:="search")>
-    <Parameter(Mandatory:=True, ParameterSetName:="name")>
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
     Public Property TemplateLibrary As String
 
     <Parameter(ParameterSetName:="search")>
-    <Parameter(Mandatory:=True, ParameterSetName:="name")>
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
     Public Property TemplateGroup As String
 
     <[Alias]("TemplateName")>
-    <Parameter(Mandatory:=True, ParameterSetName:="name")>
+    <Parameter(Mandatory:=True, ParameterSetName:="name", ValueFromPipelineByPropertyName:=True)>
     Public Property Name As String
 
-    <Parameter(Mandatory:=True, ParameterSetName:="id", ValueFromPipelineByPropertyName:=True)>
-    Public Property TemplateId As Guid
+    <[Alias]("TemplateId")>
+    <Parameter(Mandatory:=True, ParameterSetName:="id", ValueFromPipeline:=True)>
+    Public Property Id As Guid
+
+    <Parameter(Mandatory:=True, ParameterSetName:="obj", ValueFromPipeline:=True)>
+    Public Property InputObject As TemplateGroup
 
     <Parameter()>
     Public Property IncludeContent As SwitchParameter
 
-    Private idList As New List(Of Guid)
     Protected Overrides Sub ProcessRecord()
-        If ParameterSetName = "id" Then idList.Add(TemplateId)
-    End Sub
-
-    Protected Overrides Sub EndProcessing()
         'MergeOption must be changed if the Source/Assemble (DOCX/DLL) will also be returned
         If IncludeContent Then xdp.MergeOption = Microsoft.OData.Client.MergeOption.OverwriteChanges
         Try
             Dim query = xdp.Templates.Expand(Function(x) x.TemplateGroup.TemplateLibrary).AsQueryable
 
-            If Not String.IsNullOrWhiteSpace(TemplateGroup) Then
-                query = query.Where(Function(x) x.TemplateGroup.Name.ToUpper.Equals(TemplateGroup.ToUpper))
-            End If
-
-            If Not String.IsNullOrWhiteSpace(TemplateLibrary) Then
-                query = query.Where(Function(x) x.TemplateGroup.TemplateLibrary.Name.ToUpper.Equals(TemplateLibrary.ToUpper))
-            End If
-
-            If ParameterSetName = "search" Then
-                If Not String.IsNullOrWhiteSpace(Search) Then
-                    query = query.Where(Function(x) x.Name.Contains(Search))
-                End If
-                For Each t In GenerateResults(query, "Template")
+            If ParameterSetName = "obj" Then
+                'Return all templates for a given templateGroup
+                For Each t In GenerateResults(query.Where(Function(x) x.TemplateGroupId = InputObject.TemplateGroupId), "Template")
                     Output(t)
                 Next
             Else
-                Dim t As Template
-                If ParameterSetName = "name" Then
-                    t = ExecuteWithTimeout(query.Where(Function(x) x.Name.ToUpper.Equals(Name.ToUpper)).FirstOrDefaultAsync)
-                Else
-                    For Each id In idList
-                        Try
-                            t = ExecuteWithTimeout(query.Where(Function(x) x.TemplateId = id).FirstOrDefaultAsync)
-                        Catch ex As Exception
-                            WriteError(StandardErrors.XDPMissing("Template", id.ToString, ex))
-                        End Try
-                    Next
+                If Not String.IsNullOrWhiteSpace(TemplateGroup) Then
+                    query = query.Where(Function(x) x.TemplateGroup.Name.ToUpper.Equals(TemplateGroup.ToUpper))
                 End If
 
-                Output(t)
+                If Not String.IsNullOrWhiteSpace(TemplateLibrary) Then
+                    query = query.Where(Function(x) x.TemplateGroup.TemplateLibrary.Name.ToUpper.Equals(TemplateLibrary.ToUpper))
+                End If
+
+                If ParameterSetName = "search" Then
+                    If Not String.IsNullOrWhiteSpace(Search) Then
+                        query = query.Where(Function(x) x.Name.Contains(Search))
+                    End If
+                    For Each t In GenerateResults(query, "Template")
+                        Output(t)
+                    Next
+                Else
+                    Dim t As Template
+                    If ParameterSetName = "name" Then
+                        t = ExecuteWithTimeout(query.Where(Function(x) x.Name.ToUpper.Equals(Name.ToUpper)).FirstOrDefaultAsync)
+                    Else
+                        Try
+                            t = ExecuteWithTimeout(query.Where(Function(x) x.TemplateId = Id).FirstOrDefaultAsync)
+                        Catch ex As Exception
+                            WriteError(StandardErrors.XDPMissing("Template", Id.ToString, ex))
+                        End Try
+                    End If
+
+                    Output(t)
+                End If
             End If
         Finally
             'Revert the MergeOption if it was changed
