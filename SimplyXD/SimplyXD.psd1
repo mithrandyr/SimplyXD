a895eb69acfed97495347d2a3927e0de36fb8eaa
mithrandyr <mithrandyr@users.noreply.github.com>
2024-02-15 12:11:32 -0500

    cleanup moduleFolder

--- SimplyXD/SimplyXD.psd1
+++ SimplyXD/SimplyXD.psd1
@@ -9,7 +9,7 @@
 @{
 
 # Script module or binary module file associated with this manifest.
-RootModule = 'dll\SimplyXD.dll'
+RootModule = 'SimplyXD.dll'
 
 # Version number of this module.
 ModuleVersion = '1.3.2024.52'
--- default.build.ps1
+++ default.build.ps1
@@ -3,23 +3,31 @@ New-Alias -Name HV -Value (Resolve-Path HandleVerbose.ps1)
 $moduleName = "SimplyXD"
 if(-not $version) {
     $version = [Version](Import-PowerShellDataFile -Path "$moduleName\$moduleName.psd1")["ModuleVersion"]
-    $version = [version]::new($version.Major, $version.Minor, (Get-Date).ToString("yyyy"), $version.Revision + 1).tostring()
+    $version = [version]::new($version.Major, $version.Minor, $version.Revision + 1).tostring()
 }
 
+task Clean {
+    remove "$moduleName\bin"
+    remove "$module.Cmdlets.dll"
+}
 task Build -If (-not $RebuildDocs) {
     Invoke-Build -File "BinarySrc\source.build.ps1" -Version $Version
-    remove "$ModuleName\dll"
-    New-Item -Path "$ModuleName\dll" -ItemType Directory | Out-Null
+    Get-ChildItem "BinarySrc\output" | Copy-Item "$moduleName"
+    
+    <#
+    remove "$moduleName\dll"
+    New-Item -Path "$moduleName\dll" -ItemType Directory | Out-Null
     Get-ChildItem -Path "BinarySrc\output" -Filter *.dll |
         Copy-Item -Destination "$moduleName\dll"
     
     remove "BinarySrc\output"
+    #>
 }
 
 task GenerateDocs {
     Start-Job -ScriptBlock {
             Set-Location $using:BuildRoot
-            Import-Module ".\$using:moduleName\dll\$using:moduleName.dll" -Verbose:$false
+            Import-Module ".\$using:moduleName\$using:moduleName.Cmdlets.dll" -Verbose:$false
             
             if(-not (Test-Path "Docs")) {
                 New-MarkdownHelp -Module $using:moduleName -OutputFolder Docs -AlphabeticParamsOrder -WithModulePage
@@ -36,7 +44,7 @@ task updateManifest {
     Import-Module PowerShellGet -Verbose:$false
     $cmdlets = Start-Job -ScriptBlock {
             Set-Location $using:BuildRoot  
-            Import-Module ".\$using:moduleName\dll\$using:moduleName.dll"    
+            Import-Module ".\$using:moduleName\$using:moduleName.Cmdlets.dll"    
             Get-Command -Module $using:moduleName
         } |
         Receive-Job -AutoRemoveJob -wait |
@@ -52,4 +60,4 @@ task revisionCommit {
 } -If ($CommitRevision -and -not $RebuildDocs)
 
 
-task . Build, updateManifest, GenerateDocs, revisionCommit
+task . Clean, Build, updateManifest, GenerateDocs, revisionCommit
