12001d1d3ea876c2acb98243c79ea1fea446f09a
mithrandyr <mithrandyr@users.noreply.github.com>
2024-02-21 15:26:43 -0500

    minor adjustmentgs

--- BinarySrc/SimplyXD.Cmdlets/Extensions.vb
+++ BinarySrc/SimplyXD.Cmdlets/Extensions.vb
@@ -1,13 +1,7 @@
 ﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.OData.Client
 
 Module Extensions
-
-    <Extension()>
-    Public Function AsPSObject(tg As TemplateGroup) As PSObject
-        Dim x = PSObject.AsPSObject(tg)
-        x.Properties.Add(New PSScriptProperty("TemplateLibraryName", ScriptBlock.Create("if($this.TemplateLibrary) { $this.TemplateLibrary.Name }")))
-        Return x
-    End Function
 
     <Extension()>
     Public Function AsPSObject(tg As Template) As PSObject
@@ -15,6 +9,13 @@ Module Extensions
         x.Properties.Add(New PSScriptProperty("TemplateGroupName", ScriptBlock.Create("if($this.TemplateGroup) { $this.TemplateGroup.Name }")))
 
         x.Properties.Add(New PSScriptProperty("TemplateLibraryName", ScriptBlock.Create("if($this.TemplateGroup -and $this.TemplateGroup.TemplateLibrary) { $this.TemplateGroup.TemplateLibrary.Name }")))
+        Return x
+    End Function
+
+    <Extension()>
+    Public Function AsPSObject(x As IBaseEntityType)
+
+
         Return x
     End Function
 End Module
