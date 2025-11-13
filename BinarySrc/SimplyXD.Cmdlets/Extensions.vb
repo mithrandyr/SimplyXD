8b04f3235a0216ca05d77ef065bc5b0152e71fa3
mithrandyr <mithrandyr@users.noreply.github.com>
2025-11-13 17:37:17 -0500

    added extensions

--- BinarySrc/SimplyXD.Cmdlets/Extensions.vb
+++ BinarySrc/SimplyXD.Cmdlets/Extensions.vb
@@ -13,9 +13,17 @@ Module Extensions
     End Function
 
     <Extension()>
-    Public Function AsPSObject(x As IBaseEntityType)
+    Public Function AsPSObject(x As Object) As PSObject
+        If x Is GetType(PSObject) Then
+            Return x
+        Else
+            Return PSObject.AsPSObject(x)
+        End If
+    End Function
 
-
-        Return x
+    <Extension()>
+    Public Function AppendProperty(obj As PSObject, name As String, value As Object) As PSObject
+        obj.Properties.Add(New PSNoteProperty(name, value))
+        Return obj
     End Function
 End Module
