dbd88d59abeb1756b978a6a03687d570b2ed1146
Mithrandyr <mithrandyr@users.noreply.github.com>
2025-11-24 13:44:47 -0500

    build update (#20)

--- default.build.ps1
+++ default.build.ps1
@@ -1,9 +1,11 @@
-param([switch]$CommitRevision, [switch]$RebuildDocs, [switch]$GenerateMAML)
+param([switch]$CommitRevision, [switch]$RebuildDocs, [switch]$GenerateMAML, [switch]$skipIncrement)
 New-Alias -Name HV -Value (Resolve-Path HandleVerbose.ps1)
 $moduleName = "SimplyXD"
 if(-not $version) {
     $version = [Version](Import-PowerShellDataFile -Path "$moduleName\$moduleName.psd1")["ModuleVersion"]
-    $version = [version]::new($version.Major, $version.Minor, $version.Build + 1).tostring()
+    if(-not $skipIncrement) {
+        $version = [version]::new($version.Major, $version.Minor, $version.Build + 1).tostring()
+    }
 }
 
 task Clean -If (-not $RebuildDocs) {
@@ -59,6 +61,6 @@ task updateManifest {
 
 task revisionCommit {
     exec { git commit "$moduleName/$moduleName.psd1" -m "Updating version To $version" } | HV "Incrementing Version ($version) and Git Commit"
-} -If ($CommitRevision -and -not $RebuildDocs)
+} -If ($CommitRevision -and -not $RebuildDocs -and -not $skipIncrement)
 
 task . Clean, Build, updateManifest, GenerateDocs, MAML, revisionCommit
